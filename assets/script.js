b89662ad7732ed1097195866242963f727b43550
Jennifer Ross <jjannross@gmail.com>
2021-01-15 18:55:44 -0500

    corrected local storage, added data attribute, textarea id, and getItems from local storage

--- assets/script.js
+++ assets/script.js
@@ -7,26 +7,27 @@ var time =
   today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date + " " + time;
 dateEl.text(dateTime);
-console.log(dateTime);
 
 // var saveBtn = $(".saveBtn"); dont remember what this is for
 
 //save text area input to local storage when button is clicked.
-saveBtn.on("click", function (event) {
+$(".saveBtn").on("click", function (event) {
   //   $(".description").text();
-
+  var hour = $(event.target).data("hour");
+  var text = $(event.target).parent().siblings(".description").children().val();
   //when you click
+
+  var time = JSON.parse(localStorage.getItem("time"));
   //read from local storage and parse it back JSON.parse
   //change the object,
+  time[hour] = text;
   //write whole object back and stringify
-
-  var text = $(event.target).parent().siblings(".description").children().val();
-  console.log(text);
+  localStorage.setItem("time", JSON.stringify(time));
 });
 
 if (localStorage.getItem("time") === null) {
   var timeBlocks = {
-    8: "test",
+    8: "",
     9: "",
     10: "",
     11: "",
@@ -40,10 +41,31 @@ if (localStorage.getItem("time") === null) {
 
   localStorage.setItem("time", JSON.stringify(timeBlocks));
 }
+var time = JSON.parse(localStorage.getItem("time"));
+$("#8").val(time[8]);
+$("#9").val(time[9]);
+//to include:
+// will i then use the .data() to get the data from the above object?
+
+//data-hour:8 attribute
 
 //will need to creatte some sort of data-attribute for data hour
 
-//use day.js CDN to capture hour then assign classes for .past, . present, . future using if statement will
+//use day.js to capture hour then assign classes for .past, . present, . future using if statement will
+
+// var now = dayjs(){
+
+//   for (var i = 0; i < now.length; i++) {
+//     var now= now[i]
+//   dayjs().hour()
+
+//   gethours
+//   if
+//   past
+//   present
+//   future
+//   div.setAttribute(".past", i);
+// }
 
 // have save button, save it locally
 // function storedescriptions() {
