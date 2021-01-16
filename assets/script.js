2ce5091e464e035f3f49983a4a6c897fd83137d1
Jennifer Ross <jjannross@gmail.com>
2021-01-15 21:12:17 -0500

    corrected local storage, began assigning classes for styling and hour

--- assets/script.js
+++ assets/script.js
@@ -3,25 +3,40 @@ var dateEl = $("#currentDay");
 var today = new Date();
 var date =
   today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-var time =
-  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var time = today.getHours() + ":" + today.getMinutes();
 var dateTime = date + " " + time;
 dateEl.text(dateTime);
 
-// var saveBtn = $(".saveBtn"); dont remember what this is for
+// add classes to determine current hour
+
+var d = new Date();
+var t = d.getHours();
+var tB = $("#data-hour");
+console.log(t);
+console.log(tB);
+
+// if  === t
+// currentTime=.getHours
+// if data-hour is < current time, then past class
+// if data-hour = time then present class
+// if data-hour is > then future class
+
+// var now = dayjs(){
+
+//   for (var i = 0; i < now.length; i++) {
+//     var now= now[i]
+//   dayjs().hour()
 
 //save text area input to local storage when button is clicked.
 $(".saveBtn").on("click", function (event) {
   //   $(".description").text();
   var hour = $(event.target).data("hour");
   var text = $(event.target).parent().siblings(".description").children().val();
-  //when you click
 
   var time = JSON.parse(localStorage.getItem("time"));
   //read from local storage and parse it back JSON.parse
   //change the object,
   time[hour] = text;
-  //write whole object back and stringify
   localStorage.setItem("time", JSON.stringify(time));
 });
 
@@ -44,45 +59,13 @@ if (localStorage.getItem("time") === null) {
 var time = JSON.parse(localStorage.getItem("time"));
 $("#8").val(time[8]);
 $("#9").val(time[9]);
-//to include:
-// will i then use the .data() to get the data from the above object?
+$("#10").val(time[11]);
+$("#11").val(time[17]);
+$("#12").val(time[13]);
+$("#13").val(time[12]);
+$("#14").val(time[14]);
+$("#15").val(time[16]);
+$("#16").val(time[15]);
+$("#17").val(time[17]);
 
-//data-hour:8 attribute
 
-//will need to creatte some sort of data-attribute for data hour
-
-//use day.js to capture hour then assign classes for .past, . present, . future using if statement will
-
-// var now = dayjs(){
-
-//   for (var i = 0; i < now.length; i++) {
-//     var now= now[i]
-//   dayjs().hour()
-
-//   gethours
-//   if
-//   past
-//   present
-//   future
-//   div.setAttribute(".past", i);
-// }
-
-// have save button, save it locally
-// function storedescriptions() {
-//   localStorage.setItem(".description", input);
-// }
-// //do I need to JSON.stringify(descriptions) here?
-
-// // function init() {
-// //   // Get stored descriptions from localStorage
-// //   // Parsing the JSON string to an object
-// //   var storedTodos = JSON.parse(localStorage.getItem(".descriptions"));
-
-//   // If descriptions were retrieved from localStorage, update the todos array to it
-//   if (storeddescriptions !== null) {
-//     descriptions = storeddescriptions;
-//   }
-
-//   // Render todos to the DOM
-//   renderdescriptions();
-// }
